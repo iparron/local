8824a02bf6372f1e4bd9ea0ec7c6d23266e5b291
iker <iparron@dibal.com>
2018-02-21 11:21:17 +0100

    Traductor added

---
+++ Traductor/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Traductor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            CultureInfo currentCulture = Thread.CurrentThread.CurrentCulture;
+            this.button1.Text = translate.hola;
+        }
+    }
+}
